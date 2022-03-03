53677fe462fe29fb7418375401fc2d00c452d2a4
Harlan Howe <hhowe7@gatech.edu>
2022-03-03 14:42:32 -0600

    Added additional TODO comments for sake of clarity!

--- src/TextTwister.java
+++ src/TextTwister.java
@@ -14,4 +14,13 @@ public class TextTwister
         return result;
     }
 
+    // TODO: write a recursive method that receives a String for
+    //  the "prefix" and the String for the "pool" and returns
+    //  an ArrayList of all the possible "words" that start with the
+    //  prefix and contain some, none, or all of the letters in the pool.
+
+    // TODO: (optional) given an ArrayList of "words," returns another
+    //  ArrayList of all the words on this list that are actually found
+    //  in the dictionary.
+
 }
